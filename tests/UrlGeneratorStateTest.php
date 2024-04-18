b3e272db1df61a0ebd0af862177d9077f6ac6732
Miura Toshitaka <miura@bmj-inc.com>
2024-04-18 11:10:26 +0900

    Added a failing test

--- tests/UrlGeneratorStateTest.php
+++ tests/UrlGeneratorStateTest.php
@@ -2,36 +2,41 @@
 
 namespace Laravel\Octane\Tests;
 
+use Illuminate\Auth\Middleware\Authenticate;
+use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Middleware\SubstituteBindings;
-use Illuminate\Support\Facades\URL;
+use Orchestra\Testbench\Http\Middleware\RedirectIfAuthenticated;
 
 class UrlGeneratorStateTest extends TestCase
 {
     public function test_url_generator_creates_correct_urls_across_subsequent_with_bind()
     {
         [$app, $worker, $client] = $this->createOctaneContext([
-            Request::create('foo/bar', 'GET'),
-            Request::create('foo/wibble', 'GET'),
+            Request::create('foo/bar/home', 'GET'),
+            Request::create('foo/baz/home', 'GET'),
         ]);
 
-        $app['router']->bind('param', function (string $value)
+        $app['router']->bind('param', function (string $value) use ($app)
         {
-            URL::defaults(['param' => strtoupper($value)]);
+            $app['url']->defaults(['param' => strtoupper($value)]);
 
             return $value;
         });
 
-        $app['router']->get('foo/{param}', [
-            'middleware' => SubstituteBindings::class,
-            'uses' => function () {
-                return URL::getDefaultParameters()['param'];
-            },
-        ]);
+        $app['router']->get('/foo/{param}/login', function (Application $app)
+        {
+            return $app['url']->getDefaultParameters()['param'];
+        })->middleware([SubstituteBindings::class, RedirectIfAuthenticated::class])->name('login');
+
+        $app['router']->get('/foo/{param}/home', function (Application $app)
+        {
+            return $app['url']->getDefaultParameters()['param'];
+        })->middleware([SubstituteBindings::class, Authenticate::class])->name('home');
 
         $worker->run();
 
         $this->assertEquals('BAR', $client->responses[0]->getContent());
-        $this->assertEquals('WIBBLE', $client->responses[1]->getContent());
+        $this->assertEquals('BAZ', $client->responses[1]->getContent());
     }
 }
